3e2d25010215a6aaa4ad91c2613b299c475c6a4c
Ilhamlafeer <120272556+Ilhamlafeer@users.noreply.github.com>
2024-12-12 16:44:17 +0000

    cart php file created

--- index.php
+++ index.php
@@ -84,39 +84,6 @@
         </div>
     </section>
 
-    <!-- Cart Section Design -->
-<section id="cart" class="cart">
-    <h2>Your Cart</h2>
-    
-    <table class="cart-table">
-        <thead>
-            <tr>
-                <th>S.No</th>
-                <th>Name</th>
-                <th>Quantity</th>
-                <th>Unit Price</th>
-                <th>Price</th>
-                <th>Action</th> <!-- Column header for the Remove button -->
-            </tr>
-        </thead>
-        <tbody class="cart-items">
-            <!-- Cart Items will be populated here dynamically -->
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="4">Total:</td>
-                <td id="total-price">$0.00</td> 
-                <td></td> 
-            </tr>
-        </tfoot>
-    </table>
-
-    <div class="btn-box">
-        <button class="btn" onclick="checkout()">Checkout</button>
-    </div>
-</section>
-
-
     <!-- Footer Design -->
     <footer class="footer">
         <div class="footer-text">
