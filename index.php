04f713bd9cc75d18b8729271a0fe6b2db6b46176
Ilhamlafeer <120272556+Ilhamlafeer@users.noreply.github.com>
2024-12-12 16:38:57 +0000

    about php file created

--- index.php
+++ index.php
@@ -84,27 +84,6 @@
         </div>
     </section>
 
-        
-
-
-    
-
-    <!-- About Section Design -->
-    <section class="about" id="about">
-        <h2 class="heading">About <span>DBay</span></h2>
-
-        <div class="about-content">
-            <p>
-                DBay is a digital marketplace dedicated to providing a platform for startup developers and digital
-                creators. We offer a diverse range of digital products, including apps, online courses, eBooks, and
-                more. Our mission is to empower creators to reach a wider audience and provide customers with access to
-                the latest innovations.
-            </p>
-            <div class="btn-box btns">
-                <a href="#contact" class="btn">Learn More</a>
-            </div>
-        </div>
-    </section>
 
     <!-- Contact Section Design -->
     <section class="contact" id="contact">
