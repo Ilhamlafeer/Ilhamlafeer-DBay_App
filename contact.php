b025c0abd02b8bd763cd5040c515ed7790e1c70f
Ilhamlafeer <120272556+Ilhamlafeer@users.noreply.github.com>
2024-12-12 16:40:24 +0000

    contact php file created

---
+++ contact.php
@@ -0,0 +1,33 @@
+<!-- Contact Section Design -->
+<link rel="stylesheet" href="styles.css">
+<!-- Boxicons -->
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+<section class="contact" id="contact">
+        <h2 class="heading">Contact <span>Us</span></h2>
+
+        <form action="#" method="post">
+            <div class="input-box">
+                <div class="input-field">
+                    <input type="text" name="name" placeholder="Full Name" required>
+                    <span class="focus"></span>
+                </div>
+
+                <div class="input-field">
+                    <input type="email" name="email" placeholder="Email Address" required>
+                    <span class="focus"></span>
+                </div>
+            </div>
+
+            <div class="textarea-field">
+                <textarea name="message" cols="30" rows="10" placeholder="Your Message" required></textarea>
+                <span class="focus"></span>
+            </div>
+
+            <div class="btn-box btns">
+                <button type="submit" class="btn">Submit</button>
+            </div>
+        </form>
+    </section>
+
+    <script src="script.js"></script>
--- index.php
+++ index.php
@@ -84,35 +84,6 @@
         </div>
     </section>
 
-
-    <!-- Contact Section Design -->
-    <section class="contact" id="contact">
-        <h2 class="heading">Contact <span>Us</span></h2>
-
-        <form action="#" method="post">
-            <div class="input-box">
-                <div class="input-field">
-                    <input type="text" name="name" placeholder="Full Name" required>
-                    <span class="focus"></span>
-                </div>
-
-                <div class="input-field">
-                    <input type="email" name="email" placeholder="Email Address" required>
-                    <span class="focus"></span>
-                </div>
-            </div>
-
-            <div class="textarea-field">
-                <textarea name="message" cols="30" rows="10" placeholder="Your Message" required></textarea>
-                <span class="focus"></span>
-            </div>
-
-            <div class="btn-box btns">
-                <button type="submit" class="btn">Submit</button>
-            </div>
-        </form>
-    </section>
-
     <!-- Cart Section Design -->
 <section id="cart" class="cart">
     <h2>Your Cart</h2>
